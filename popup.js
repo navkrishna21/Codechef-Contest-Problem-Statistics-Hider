36088805d31e17f3f914a678e4cae201dffd21d7
Navkrishna <navkrishna21@github.com>
2019-10-18 08:09:17 +0530

    Text box for status included

--- popup.js
+++ popup.js
@@ -2,8 +2,12 @@ $(function(){
 
 	chrome.storage.sync.get('toggle' , function(state){
 		
-		if(state.toggle =='on')
-			$('.toggle-button').attr('checked', true);
+		if(state.toggle =='on'){
+			$('#status').text('Submissions are hidden');
+			$('.toggle-button').prop('checked', true);
+		}
+		else
+			$('#status').text('Toggle to hide submissions');
 	})
 
 	$(".toggle-button").change(function() {
@@ -14,7 +18,9 @@ $(function(){
 
 	    		chrome.tabs.query({active:true,currentWindow: true}, function(tabs){
 
-	            	chrome.tabs.sendMessage(tabs[0].id, {todo: "hideSubmissions"});
+	            	chrome.tabs.sendMessage(tabs[0].id, {todo: "hideSubmissions"},function(){
+	            		$('#status').text('Submissions are hidden')
+	            	});
 	        	
 	        	});
 
@@ -27,7 +33,9 @@ $(function(){
 
 	    		chrome.tabs.query({active:true,currentWindow: true}, function(tabs){
 
-	            	chrome.tabs.sendMessage(tabs[0].id, {todo: "showSubmissions"});
+	            	chrome.tabs.sendMessage(tabs[0].id, {todo: "showSubmissions"},function(){
+	            		$('#status').text('Toggle to hide submissions')
+	            	});
 	        	
 	        	});
 

8ea2a4222c264508b1dc7bc7334635f1b3986427
Navkrishna <navkrishna21@github.com>
2019-10-18 05:41:15 +0530

    toggling off functionality added

--- content.js
+++ content.js
@@ -6,11 +6,22 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     if (request.todo == "hideSubmissions")
     {
 
-    	 	 $(document).ready(function() {
-				
-				$('td.num').css('display','none');    
+	 	$(document).ready(function() {
 			
-			});
+			$('td.num').css('display','none');    
+		
+		});
+					 	
+    	 	//$('#contest-ranks').css('display','none !important');			 	
+    }
+    else if (request.todo == "showSubmissions")
+    {
+
+	 	$(document).ready(function() {
+			
+			$('td.num').css('display','table-cell');    
+		
+		});
 					 	
     	 	//$('#contest-ranks').css('display','none !important');			 	
     }
--- popup.js
+++ popup.js
@@ -23,7 +23,15 @@ $(function(){
     	}
     	else{
 
-    		chrome.storage.sync.set({'toggle' : 'off' });
+    		chrome.storage.sync.set({'toggle' : 'off' } ,function(){
+
+	    		chrome.tabs.query({active:true,currentWindow: true}, function(tabs){
+
+	            	chrome.tabs.sendMessage(tabs[0].id, {todo: "showSubmissions"});
+	        	
+	        	});
+
+    		});
 
     	}
 	});

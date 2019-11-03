50bc6d2f63f48b800249a0cb17c8d8e1a9627a5d
Navkrishna <navkrishna.official@gmail.com>
2019-11-03 23:58:29 +0530

    Added Url pattern matching

--- content.js
+++ content.js
@@ -1,48 +1,54 @@
+var url_re = /^(http|https):\/\/(www.codechef|codechef)\.com\/[A-Z]+[0-9]*[A-B]*(\?[A-Za-z0-9\?&=_-]+)?$/
+
 chrome.runtime.sendMessage({todo: "showPageAction"});
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
  
+	
+	if(url_re.test(window.location.toString())){
 
-    if(request.todo == "hideOnLoad"){
-    	
-		var MY_SELECTOR = "td.num" // Could be any selector
+		console.log("url matched")
+	    
+	    if(request.todo == "hideOnLoad"){
+	    	
+			var MY_SELECTOR = "td.num" // Could be any selector
 
-		var observer = new MutationObserver(function(mutations){
-		 
-		    for (var mutation of mutations){
-		      // We're iterating through _all_ the elements as the parser parses them,
-		      // deciding if they're the one we're looking for.
-		      if (document.querySelector(MY_SELECTOR)){
+			var observer = new MutationObserver(function(mutations){
+			 
+			    for (var mutation of mutations){
+			      // We're iterating through _all_ the elements as the parser parses them,
+			      // deciding if they're the one we're looking for.
+			      if (document.querySelector(MY_SELECTOR)){
 
-		        $('td.num').css('display','none'); 
-				$('#contest-ranks > p').css('display','none');	
-		        chrome.runtime.sendMessage({todo: "notify"});
-		        observer.disconnect();
-		      };
-		    }
-		  
-		});
+			        $('td.num').css('display','none'); 
+					$('#contest-ranks > p').css('display','none');	
+			        chrome.runtime.sendMessage({todo: "notify"});
+			        observer.disconnect();
+			      };
+			    }
+			  
+			});
 
-		observer.observe(document.documentElement, {
-		  childList: true,
-		  subtree: true
-		});
+			observer.observe(document.documentElement, {
+			  childList: true,
+			  subtree: true
+			});
 
 
-    }
-    else if (request.todo == "hideStatistics")
-    {
-		$('td.num').css('display','none'); 
-		$('#contest-ranks > p').css('display','none');				 	 			 	
-    }
-    else if (request.todo == "showStatistics")
-    {
+	    }
+	    else if (request.todo == "hideStatistics")
+	    {
+			$('td.num').css('display','none'); 
+			$('#contest-ranks > p').css('display','none');				 	 			 	
+	    }
+	    else if (request.todo == "showStatistics")
+	    {
 
-    	
-		$('td.num').css('display','table-cell');
-		$('#contest-ranks > p').css('display','block');    		
-					 	
-    	 	    	
-    }
-
+	    	
+			$('td.num').css('display','table-cell');
+			$('#contest-ranks > p').css('display','block');    		
+						 	
+	    	 	    	
+	    }
+	}
 });
